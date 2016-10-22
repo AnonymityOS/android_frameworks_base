58528904abf309d21d977e173fe8233388474f32
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2016-10-22 08:53:29 +0800

    Option to hide Adb notification icon [2/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6514,6 +6514,12 @@ public final class Settings {
         public static final String DEMO_USER_SETUP_COMPLETE
                 = "demo_user_setup_complete";
 
+         /**
+         * Whether to display the ADB notification.
+         * @hide
+         */
+        public static final String ADB_NOTIFY = "adb_notify";
+
         /**
          * Specifies whether the web action API is enabled.
          *
